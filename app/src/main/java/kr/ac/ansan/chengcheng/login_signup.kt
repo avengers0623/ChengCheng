a39c32f754121c58c4837cee03633928ff8dfae8
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-07-08 04:00:40 +0900

    ex

--- app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
@@ -1,6 +1,18 @@
 package kr.ac.ansan.chengcheng
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login_signup.*
 
 class login_signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_signup)
+
+        TextView_signup.setOnClickListener {
+         //지석 여기서 부터
+        }
+
+    }
 }
