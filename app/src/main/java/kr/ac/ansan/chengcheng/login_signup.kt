8e378b10c86bdfcc65375f021312cb5bb7d58125
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-07-08 23:13:01 +0900

    ex

--- app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
@@ -11,8 +11,16 @@ class login_signup : AppCompatActivity() {
         setContentView(R.layout.login_signup)
 
         val selfsignup = Intent(this,self_signup::class.java)
+        val activitymain = Intent(this,MainActivity::class.java)
+
+        //회원가입 창
         TextView_signup.setOnClickListener {
             startActivity(selfsignup)
+        }
+
+        // 임시로 로그인 버튼 눌렸을때 메인 화면으로
+        login_button.setOnClickListener {
+            startActivity(activitymain)
         }
 
     }
