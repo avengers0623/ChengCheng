e648b086c4cc64ffcd433e8028b96c0b514ac713
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-07-15 01:21:37 +0900

    ex

--- app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
@@ -1,10 +1,13 @@
 package kr.ac.ansan.chengcheng
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.login_signup.*
@@ -13,21 +16,32 @@ class login_signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_signup)
-
+        val context: Context = applicationContext
 
         val selfsignup = Intent(this,self_signup::class.java)
         val activitymain = Intent(this,MainActivity::class.java)
+        val socialSignup = Intent(this,social_signup::class.java)
 
-        /*button_kakao_login.setOnClickListener {
-            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+
+        button_kakao_login.setOnClickListener {
+            Toast.makeText(context, "dadsdasdasd", Toast.LENGTH_SHORT).show()
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
                 if (error != null) {
                     Log.e(TAG, "로그인 실패", error)
                 }
                 else if (token != null) {
                     Log.i(TAG, "로그인 성공 ${token.accessToken}")
+                    startActivity(socialSignup)
                 }
             }
-        }*/
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(context, callback = callback)
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            }
+        }
         //회원가입 창
         TextView_signup.setOnClickListener {
             startActivity(selfsignup)
@@ -39,4 +53,7 @@ class login_signup : AppCompatActivity() {
         }
 
     }
+
+
+
 }
--- app/src/main/java/kr/ac/ansan/chengcheng/social_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/social_signup.kt
@@ -1,8 +1,16 @@
 package kr.ac.ansan.chengcheng
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class social_signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.social_signup)
+
+
+
+    }
 
 
 }
