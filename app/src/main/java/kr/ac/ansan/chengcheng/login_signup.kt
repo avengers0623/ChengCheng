b236cb8ba94079463e2d266caa01c5ae1561bf7c
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-07-15 03:16:07 +0900

    ex

--- app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
@@ -3,12 +3,12 @@ package kr.ac.ansan.chengcheng
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.login_signup.*
 
@@ -18,6 +18,9 @@ class login_signup : AppCompatActivity() {
         setContentView(R.layout.login_signup)
         val context: Context = applicationContext
 
+        val intent = Intent(context, social_signup::class.java)
+
+
         val selfsignup = Intent(this,self_signup::class.java)
         val activitymain = Intent(this,MainActivity::class.java)
         val socialSignup = Intent(this,social_signup::class.java)
@@ -26,20 +29,38 @@ class login_signup : AppCompatActivity() {
         button_kakao_login.setOnClickListener {
             Toast.makeText(context, "dadsdasdasd", Toast.LENGTH_SHORT).show()
             val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 if (error != null) {
                     Log.e(TAG, "로그인 실패", error)
                 }
                 else if (token != null) {
                     Log.i(TAG, "로그인 성공 ${token.accessToken}")
-                    startActivity(socialSignup)
+                    //intent = Intent(Intent.ACTION_VIEW,  Uri.parse(KakaoTalkLinkProtocol.TALK_MARKET_URL_PREFIX_2 + makeReferrer()))
+                    startActivity(intent)
                 }
             }
 
 // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-                UserApiClient.instance.loginWithKakaoTalk(context, callback = callback)
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
             } else {
-                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+
+            // 사용자 정보 요청 (기본)
+            UserApiClient.instance.me { user, error ->
+                if (error != null) {
+                    Log.e(TAG, "사용자 정보 요청 실패", error)
+                }
+                else if (user != null) {
+                    Log.i(TAG, "사용자 정보 요청 성공" +
+                            "\n회원번호: ${user.id}" +
+                            "\n이메일: ${user.kakaoAccount?.email}" +
+                            "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                            "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"+
+                          "\n성별: ${user.kakaoAccount?.gender}")
+
+                }
             }
         }
         //회원가입 창
--- app/src/main/java/kr/ac/ansan/chengcheng/social_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/social_signup.kt
@@ -1,12 +1,25 @@
 package kr.ac.ansan.chengcheng
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.android.synthetic.main.social_signup.*
 
 class social_signup : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.social_signup)
+        val mainPage = Intent(this, kr.ac.ansan.chengcheng.MainPage::class.java)
+
+
+        start.setOnClickListener {
+            startActivity(mainPage)
+
+        }
 
 
 
