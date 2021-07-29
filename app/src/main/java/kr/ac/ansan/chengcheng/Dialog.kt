34abf541bf36d72bf537da3358d0f530f58c872b
n06578 <n06578@gmail.com>
2021-07-30 01:48:50 +0900

    팝업

--- app/src/main/java/kr/ac/ansan/chengcheng/Dialog.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/Dialog.kt
@@ -24,7 +24,7 @@ class Dialog(context : Context) {
 //        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(R.layout.dialog)     //다이얼로그에 사용할 xml 파일을 불러옴
         dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
-        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         lblDesc = dlg.findViewById(R.id.content)
         lblDesc.text = content
 
