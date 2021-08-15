698cc49fd81e787459e812ff9ee0e0c0e874d09b
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-08-16 01:00:24 +0900

    구글 로그인 연동 완료

--- app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/login_signup.kt
@@ -6,14 +6,19 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -22,9 +27,14 @@ import com.kakao.sdk.user.UserApiClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.login_signup.*
 
-class login_signup : AppCompatActivity() {
+class login_signup : AppCompatActivity(), View.OnClickListener {
+    // [START declare_auth]
+    private lateinit var firebaseAuth: FirebaseAuth
+    // [END declare_auth]
 
+    private lateinit var googleSignInClient: GoogleSignInClient
 
+    private val RC_SIGN_IN = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +43,32 @@ class login_signup : AppCompatActivity() {
 
         val intent = Intent(context, social_signup::class.java)
 
-        val selfsignup = Intent(this,self_signup::class.java)
-        val activitymain = Intent(this,MainActivity::class.java)
-        val socialSignup = Intent(this,social_signup::class.java)
+        val selfsignup = Intent(this, self_signup::class.java)
+        val activitymain = Intent(this, MainActivity::class.java)
+
+
+
 
         Glide.with(this)
             .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnCZj8tODbi4JNvIG4p4OJ3uH4wQthqjqOBw&usqp=CAU")
             .into(logo)
 
 
+        button_google_login.setOnClickListener {
+            signIn()
+        }
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            //'R.string.default_web_client_id' 에는 본인의 클라이언트 아이디를 넣어주시면 됩니다.
+            //저는 스트링을 따로 빼서 저렇게 사용했지만 스트링을 통째로 넣으셔도 됩니다.
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //firebase auth 객체
+        firebaseAuth = FirebaseAuth.getInstance()
 
         button_kakao_login.setOnClickListener {
             Toast.makeText(context, "dadsdasdasd", Toast.LENGTH_SHORT).show()
@@ -51,8 +77,7 @@ class login_signup : AppCompatActivity() {
                 if (error != null) {
                     Log.e(TAG, "로그인 실패", error)
                     Toast.makeText(context, "실패", Toast.LENGTH_SHORT).show()
-                }
-                else if (token != null) {
+                } else if (token != null) {
                     Log.i(TAG, "로그인 성공 ${token.accessToken}")
                     //intent = Intent(Intent.ACTION_VIEW,  Uri.parse(KakaoTalkLinkProtocol.TALK_MARKET_URL_PREFIX_2 + makeReferrer()))
                     startActivity(intent)
@@ -77,14 +102,112 @@ class login_signup : AppCompatActivity() {
         }
 
 
-
         // 임시로 로그인 버튼 눌렸을때 메인 화면으로
         login_button.setOnClickListener {
             startActivity(activitymain)
         }
+    }
+
+        public override fun onStart() {
+            super.onStart()
+            val account = GoogleSignIn.getLastSignedInAccount(this)
+            if(account!==null){ // 이미 로그인 되어있을시 바로 메인 액티비티로 이동
+                toMainActivity(firebaseAuth.currentUser)
+            }
+        } //onStart End
+
+        // onActivityResult
+        public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+            if (requestCode == RC_SIGN_IN) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)
+                    firebaseAuthWithGoogle(account!!)
+
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("LoginActivity", "Google sign in failed", e)
+                }
+            }
+        } // onActivityResult End
+
+        // firebaseAuthWithGoogle
+        private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+            val socialSignup = Intent(this, social_signup::class.java)
+
+            Log.d("LoginActivity", "firebaseAuthWithGoogle:" + acct.id!!)
+
+            //Google SignInAccount 객체에서 ID 토큰을 가져와서 Firebase Auth로 교환하고 Firebase에 인증
+            val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+            firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.w("LoginActivity", "firebaseAuthWithGoogle 성공", task.exception)
+                        toMainActivity(firebaseAuth?.currentUser)
+                        startActivity(socialSignup)
+                    } else {
+                        Log.w("LoginActivity", "firebaseAuthWithGoogle 실패", task.exception)
+
+                    }
+
+                }
+        }// firebaseAuthWithGoogle END
+
+
+        // toMainActivity
+        fun toMainActivity(user: FirebaseUser?) {
+            if(user !=null) { // MainActivity 로 이동
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        } // toMainActivity End
+
+        // signIn
+        private fun signIn() {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+        // signIn End
 
 
 
+
+        private fun signOut() { // 로그아웃
+            // Firebase sign out
+            firebaseAuth.signOut()
+
+            // Google sign out
+            googleSignInClient.signOut().addOnCompleteListener(this) {
+                //updateUI(null)
+            }
+        }
+
+        private fun revokeAccess() { //회원탈퇴
+            // Firebase sign out
+            firebaseAuth.signOut()
+            googleSignInClient.revokeAccess().addOnCompleteListener(this) {
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    override fun onClick(v: View?) {
 
     }
 
