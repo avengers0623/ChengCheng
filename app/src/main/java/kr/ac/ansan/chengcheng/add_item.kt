b8f8bf1578516e704f0155a52ce03a4356d118f5
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2021-08-26 03:31:37 +0900

    add_item에 아이콘 추가 구현(미완성)

--- app/src/main/java/kr/ac/ansan/chengcheng/add_item.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/add_item.kt
@@ -1,44 +1,33 @@
 package kr.ac.ansan.chengcheng
 
+import android.annotation.SuppressLint
 import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.content.res.TypedArray
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
-import android.view.ViewGroup
-import android.widget.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.add_item.*
-import kotlinx.android.synthetic.main.alarm_dialog.*
-import kotlinx.android.synthetic.main.item_add.*
 import kr.ac.ansan.chengcheng.MainActivity.Companion.Items
 import kr.ac.ansan.chengcheng.MainActivity.Companion.adapter
+import kr.ac.ansan.chengcheng.MainActivity.Companion.database
+import kr.ac.ansan.chengcheng.MainActivity.Companion.listCntInt
+import kr.ac.ansan.chengcheng.MainActivity.Companion.nickName
+import kr.ac.ansan.chengcheng.MainActivity.Companion.userId
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-import kr.ac.ansan.chengcheng.MainActivity.Companion.database
-import kr.ac.ansan.chengcheng.MainActivity.Companion.listCnt
-import kr.ac.ansan.chengcheng.MainActivity.Companion.listCntInt
-import kr.ac.ansan.chengcheng.MainActivity.Companion.listName
-import kr.ac.ansan.chengcheng.MainActivity.Companion.nickName
-import kr.ac.ansan.chengcheng.MainActivity.Companion.userId
 
 
-class add_item :  AppCompatActivity() {
-
+class add_item : AppCompatActivity() {
     private val items: ArrayList<Data_addItem> = ArrayList()
-    private var rv1Data: MutableList<Data_addItem_1>? = null
+    var rv1Data: MutableList<Data_addItem_1>? = null
 //    private val builder = AlertDialog.Builder(this)
 //    private val inflater = this.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
 //    private val view = inflater.inflate(R.layout.user_dialog, null)
@@ -73,7 +62,6 @@ class add_item :  AppCompatActivity() {
 //        recyclerView.setAdapter(adapter)
 
 
-
         rv1Data = mutableListOf()
         createData()
 
@@ -87,23 +75,28 @@ class add_item :  AppCompatActivity() {
         rv1.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
 
-
         val dig = User_dialog(this)
         val alarm = Alarm_dialog(this)
 
 
 
-        alarm_im.setOnClickListener{
-              //alarm.start("알람")
+        alarm_im.setOnClickListener {
+            //alarm.start("알람")
             var cal = Calendar.getInstance()
             val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
                 //cal.set(Calendar.HOUR_OF_DAY, hour)
                 //cal.set(Calendar.MINUTE, minute)
-                Toast.makeText(this, "$hour : $minute",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "$hour : $minute", Toast.LENGTH_SHORT).show()
                 //            alarm.start("알람") //${SimpleDateFormat("HH:mm").format(cal.time)}
                 Log.d("ㅎ", "$hour, $minute")
             }
-            TimePickerDialog(this, timeSetListener,cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show()
+            TimePickerDialog(
+                this,
+                timeSetListener,
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                false
+            ).show()
         }
 
 
@@ -159,8 +152,6 @@ class add_item :  AppCompatActivity() {
         }
 
 
-
-
     }
 
     private fun setData(listName: String, listCntInt: Int) {
@@ -176,38 +167,45 @@ class add_item :  AppCompatActivity() {
         additem_title.setText("")
     }
 
-    private fun createData() {
-        val item1 = Data_addItem_2(R.drawable.thor, "가나다")
-        val item2 = Data_addItem_2(R.drawable.thor, "가나다")
-        val item3 = Data_addItem_2(R.drawable.thor, "가나다")
-        val item4 = Data_addItem_2(R.drawable.thor, "가나다")
-        val item5 = Data_addItem_2(R.drawable.thor, "가나다")
+    @SuppressLint("ResourceType")
+    fun createData() {
 
-        val arrFroRv2:MutableList<Data_addItem_2> = mutableListOf()
-        arrFroRv2.add(item1)
-        arrFroRv2.add(item2)
-        arrFroRv2.add(item3)
-        arrFroRv2.add(item4)
-        arrFroRv2.add(item5)
+        rv1Data = mutableListOf()
 
-        val arrFroRv3:MutableList<Data_addItem_2> = mutableListOf()
-        repeat(30) { itemIndex ->
-            arrFroRv3.add(Data_addItem_2(R.drawable.iron_man, "${itemIndex}")) }
+        val categorySub: MutableList<Data_addItem_2> = mutableListOf()
+        var typedArray: TypedArray? = null
+        typedArray = resources.obtainTypedArray(R.array.campList)
+        var max = typedArray.length()
 
-        val data_addItem_1 = Data_addItem_1("의류", arrFroRv2)
-        val data_addItem_2 = Data_addItem_1("전자기기", arrFroRv2)
-        val data_addItem_3 = Data_addItem_1("테스트", arrFroRv3)
 
-        rv1Data?.add(data_addItem_1)
-        rv1Data?.add(data_addItem_2)
-        rv1Data?.add(data_addItem_3)
+        val campNameList: Array<String> = resources.getStringArray(R.array.campNameList)
+
+
+        for (i in 0 until max) { // 0 until max면 max-1 값 리턴
+            Log.d("길이", i.toString())
+            categorySub.add(Data_addItem_2(typedArray.getResourceId(i, 0), campNameList[i]))
+        }
+
+        val categoryList: Array<String> = resources.getStringArray(R.array.category)
+        categoryList!!.forEach {
+            Log.d("카테고리", it)
+            rv1Data?.add(Data_addItem_1(it, categorySub))
+        }
+
+        /*rv1Data?.add(category1)
+        rv1Data?.add(category2)
+        rv1Data?.add(category3)
+        rv1Data?.add(category4)
+        rv1Data?.add(category5)
+        rv1Data?.add(category6)*/
     }
 
 
     fun getTime(hour: Int, minute: Int, cal: Calendar) {
 
         Toast.makeText(this, "${hour}시 ${minute}분", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this, " ${SimpleDateFormat("HH:mm").format(cal.time)}",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, " ${SimpleDateFormat("HH:mm").format(cal.time)}", Toast.LENGTH_SHORT)
+            .show()
     }
 
 
