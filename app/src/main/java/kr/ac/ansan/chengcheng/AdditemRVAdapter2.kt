68fbceed2102c6afa1d72ade0f3805b7980b532a
KwagSangJin <38849169+KwagSangJin@users.noreply.github.com>
2021-08-26 01:11:47 +0900

    아이콘

--- app/src/main/java/kr/ac/ansan/chengcheng/AdditemRVAdapter2.kt
+++ app/src/main/java/kr/ac/ansan/chengcheng/AdditemRVAdapter2.kt
@@ -1,9 +1,13 @@
 package kr.ac.ansan.chengcheng
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.GridLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.rv1_item.view.*
 import kotlinx.android.synthetic.main.rv2_item.view.*
@@ -11,12 +15,29 @@ import kotlinx.android.synthetic.main.rv2_item.view.*
 class AdditemRVAdapter2 : RecyclerView.Adapter<AdditemRVAdapter2.Rv2Holder>() {
     var data = mutableListOf<Data_addItem_2>()
 
-    inner class  Rv2Holder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class Rv2Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var onViewHolderItemClickListener: OnViewHolderItemClickListener? = null
+        private var frameLayout: FrameLayout? = null
+
+        init {
+            frameLayout = itemView.frameLayout_addItem
+
+            frameLayout!!.setOnClickListener {
+                onViewHolderItemClickListener!!.onViewHolderItemClick()
+            }
+        }
+
         fun setData(data: Data_addItem_2) {
             itemView.img_cover.setImageResource(data.getImage())
-            itemView.tv_name.text =  data.imgName
+            itemView.tv_name.text = data.imgName
+        }
+
+        @JvmName("setOnViewHolderItemClickListener2")
+        fun setOnViewHolderItemClickListener(onViewHolderItemClickListener: OnViewHolderItemClickListener?) {
+            this.onViewHolderItemClickListener = onViewHolderItemClickListener
         }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdditemRVAdapter2.Rv2Holder {
         return Rv2Holder(
@@ -25,12 +46,20 @@ class AdditemRVAdapter2 : RecyclerView.Adapter<AdditemRVAdapter2.Rv2Holder>() {
         )
     }
 
+
     override fun onBindViewHolder(holder: AdditemRVAdapter2.Rv2Holder, position: Int) {
         var item = data[position]
+
+        holder.setOnViewHolderItemClickListener {
+            notifyItemChanged(position)
+            Log.d("어댑터", "작동함")
+        }
+
         holder.apply {
             setData(item)
         }
     }
+
 
     override fun getItemCount(): Int {
         return data.size
